8479f546b0744bfb4e807522f7b42e4cca9fbffa
Paul Savala <paulsavala@gmail.com>
2019-03-21 16:15:19 -0700

    Added latex field to problem

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, g, \
-    jsonify, current_app
+    jsonify, current_app, session
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
