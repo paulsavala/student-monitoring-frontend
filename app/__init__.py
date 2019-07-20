53ddc7922c475d294624f25d4c90b43bdae46ec2
Paul Savala <paulsavala@gmail.com>
2019-07-20 12:58:46 -0700

    Working on getting db going

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask, request, current_app, redirect, url_for, current_app
+from flask import Flask, request, current_app, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user
@@ -13,8 +13,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from elasticsearch import Elasticsearch
-from config import DevConfig, ProdConfig
-
+from app.utils.utils import read_from_s3
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -28,6 +27,12 @@ babel = Babel()
 admin = Admin()
 
 def create_app():
+    print('Creating app...')
+
+    set_env_variables()
+
+    from config import DevConfig, ProdConfig
+
     if os.environ.get("APP_ENV") == "prod":
         config_class = ProdConfig
     else:
@@ -36,6 +41,15 @@ def create_app():
     app.config.from_object(config_class)
 
     db.init_app(app)
+    from app.models import User, Problem, Subject, Course, Class, Institution, Document
+    with app.app_context():
+        print(os.environ.get("DATABASE_URL"))
+        print(config_class.SQLALCHEMY_DATABASE_URI)
+        db.create_all()
+        db.session.commit()
+    if test_db():
+        create_first_user()
+
     migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
@@ -92,9 +106,74 @@ def create_app():
             app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Application startup')
+        print('app created')
 
     return app
+
+def set_env_variables():
+    print('Setting environment variables...')
+    env_variables = read_from_s3(bucket=f'problematic-{os.environ.get("APP_ENV", "dev")}-us-east-1',
+                                 key=f'configuration/{os.environ.get("APP_ENV", "dev")}-env-variables.json',
+                                 as_dict=True)
+
+    for k, v in env_variables.items():
+        os.environ[k.upper()] = v
+        print(f"Set os.environ['{k.upper()}']")
+
+    db_creds = read_from_s3(bucket=f'problematic-{os.environ.get("APP_ENV", "dev")}-us-east-1',
+                            key=f'credentials/{os.environ.get("APP_ENV", "dev")}-database.json',
+                            as_dict=True, ignore_missing=True)
+    if db_creds is not None:
+        db_conn_str = f'{db_creds["connection"]}://{db_creds["username"]}:{db_creds["password"]}@{db_creds["host"]}:{db_creds["port"]}/{db_creds["database"]}'
+        os.environ['DATABASE_URL'] = db_conn_str
+        print(f"Set os.environ['DATABASE_URL'] to {os.environ['DATABASE_URL']}")
+
+def test_db():
+    print('Testing db connection...')
+    with app.app_context():
+        try:
+            current_app.db.session.query("1").from_statement("SELECT 1").all()
+            print('DB connection succeeded')
+            return True
+        except Exception as e:
+            print('DB connection failed')
+            print(e)
+            return False
+
+def create_first_user():
+    with app.app_context():
+        from app.models import User, Problem, Subject, Course, Class, Institution, Document
+
+        db = current_app.db
+        admin_user = User.query.filter_by(admin=True).first()
+
+        if admin_user is None and os.environ.get('WERKZEUG_RUN_MAIN', 'false') == 'false':
+            app.logger.info('Creating initial admin user')
+
+            st_edwards = Institution(name="Saint Edward's University", city="Austin", state="TX", type="COLLEGE")
+
+            bucket = f'problematic-{os.environ.get("APP_ENV", "dev")}-us-east-1'
+            key = f'credentials/{os.environ.get("APP_ENV", "dev")}-credentials.json'
+            admin_credentials = read_from_s3(bucket=bucket, key=key, as_dict=True)
+
+            admin_user = User(
+                            username = admin_credentials['username'],
+                            email = admin_credentials['email'],
+                            first_name = admin_credentials['first_name'],
+                            last_name = admin_credentials['last_name'],
+                            full_name = admin_credentials['full_name'],
+                            institution_id = admin_credentials['institution_id'],
+                            admin = admin_credentials['admin']
+                            )
+            admin_user.set_password(admin_credentials['password'])
+
+            db.session.add(st_edwards)
+            db.session.add(admin_user)
+            db.session.commit()
+
+            app.logger.info('Admin user created')
+        else:
+            app.logger.info('Admin user already exists')
 
 @babel.localeselector
 def get_locale():
--- application.py
+++ application.py
@@ -1,35 +1,6 @@
-from app import create_app, db, cli
+from app import create_app, cli, db
 from app.models import User, Problem, Message, Notification, Subject, Course, Institution, Class, Document
 from app.utils.utils import read_from_s3
-
-import boto3
-import json
-import os
-
-def set_env_variables():
-    # Only set environment variables when APP_ENV is set
-    if os.environ.get('APP_ENV'):
-        print(f"APP_ENV = {os.environ.get('APP_ENV', 'dev')}")
-        env_variables = read_from_s3(bucket=f'problematic-{os.environ.get("APP_ENV", "dev")}-us-east-1',
-                                     key=f'configuration/{os.environ.get("APP_ENV", "dev")}-env-variables.json',
-                                     as_dict=True)
-
-        for k, v in env_variables.items():
-            os.environ[k.upper()] = v
-            print(f"Set os.environ['{k.upper()}']")
-
-        db_creds = read_from_s3(bucket=f'problematic-{os.environ.get("APP_ENV", "dev")}-us-east-1',
-                                key=f'credentials/{os.environ.get("APP_ENV", "dev")}-database.json',
-                                as_dict=True, ignore_missing=True)
-        if db_creds is not None:
-            db_conn_str = f'{db_creds["connection"]}://{db_creds["username"]}:{db_creds["password"]}@{db_creds["host"]}:{db_creds["port"]}/{db_creds["database"]}'
-            os.environ['DATABASE_URL'] = db_conn_str
-            print(f"Set os.environ['DATABASE_URL'] to {os.environ['DATABASE_URL']}")
-    else:
-        print("APP_ENV environment variable not set, defaulting to dev")
-        print("Environment variables will NOT be set")
-
-set_env_variables()
 
 app = create_app()
 cli.register(app)
@@ -38,35 +9,3 @@ cli.register(app)
 def make_shell_context():
     return {'db': db, 'User': User, 'Problem': Problem, 'Message': Message, 'Notification': Notification,
             'Course': Course, 'Class': Class, 'Institution': Institution, 'Document': Document}
-
-def create_first_user(app):
-    with app.app_context():
-        admin_user = User.query.filter_by(admin=True).first()
-
-        if admin_user is None and os.environ.get('WERKZEUG_RUN_MAIN', 'false') == 'false':
-            app.logger.info('Creating initial admin user')
-
-            st_edwards = Institution(name="Saint Edward's University", city="Austin", state="TX", type="COLLEGE")
-
-            bucket = f'problematic-{os.environ.get("APP_ENV", "dev")}-us-east-1'
-            key = f'credentials/{os.environ.get("APP_ENV", "dev")}-credentials.json'
-            admin_credentials = read_from_s3(bucket=bucket, key=key, as_dict=True)
-
-            admin_user = User(
-                            username = admin_credentials['username'],
-                            email = admin_credentials['email'],
-                            first_name = admin_credentials['first_name'],
-                            last_name = admin_credentials['last_name'],
-                            full_name = admin_credentials['full_name'],
-                            institution_id = admin_credentials['institution_id'],
-                            admin = admin_credentials['admin']
-                            )
-            admin_user.set_password(admin_credentials['password'])
-
-            db.session.add(st_edwards)
-            db.session.add(admin_user)
-            db.session.commit()
-
-            app.logger.info('Admin user created')
-
-create_first_user(app)
