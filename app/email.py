e882a17a86feb16c889b3937c87f33b721793793
Paul Savala <paulsavala@gmail.com>
2019-07-25 15:23:33 -0700

    removed threading for emails for debugging

--- app/email.py
+++ app/email.py
@@ -13,6 +13,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email,
-           args=(current_app._get_current_object(), msg)).start()
+    # Thread(target=send_async_email,
+    #        args=(current_app._get_current_object(), msg)).start()
+    mail.send(msg)
     current_app.logger.info(f'Password request sent to {recipients}')
