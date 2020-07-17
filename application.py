517605ca1d2bee2d5c709618d1d227b15d7225b2
Paul Savala <paulsavala@gmail.com>
2020-07-16 19:30:33 -0500

    added psycopg2-binary

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ from app import create_app, cli, db
 from app.models import Schools, CollegeOf, Departments, Courses, CourseInstances, Instructors
 
 app = create_app()
-app.ssl_context = 'adhoc'
 
 
 @app.shell_context_processor
