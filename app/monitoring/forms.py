b9e651c1ac2535807db4da3afee340668e2d1b72
Paul Savala <paulsavala@gmail.com>
2020-07-18 13:28:39 -0500

    same

--- app/monitoring/forms.py
+++ app/monitoring/forms.py
@@ -14,11 +14,11 @@ class EditCoursesFlaskForm(FlaskForm):
     refresh_courses = SubmitField('Refresh courses')
 
 
-def edit_courses_flask_form_builder(course_forms):
+def edit_courses_flask_form_builder(courses):
     class ClassesFlaskForm(EditCoursesFlaskForm):
         pass
 
-    for i, course in enumerate(course_forms):
+    for i, course in enumerate(courses):
         setattr(ClassesFlaskForm, f'is_monitored_course_{i}', BooleanField(label='Is monitored'))
         setattr(ClassesFlaskForm, f'auto_email_course_{i}', BooleanField(label='Auto email'))
         setattr(ClassesFlaskForm, f'short_name_course_{i}', StringField(label='Short name',
