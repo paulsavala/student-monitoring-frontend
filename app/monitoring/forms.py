bd143e3a58a42a26ebd06af5036c42e083e219e9
Paul Savala <paulsavala@gmail.com>
2020-07-18 13:46:17 -0500

    same

--- app/monitoring/forms.py
+++ app/monitoring/forms.py
@@ -15,20 +15,21 @@ class EditCoursesFlaskForm(FlaskForm):
     refresh_courses = SubmitField('Refresh courses')
 
 
-def edit_courses_flask_form_builder(courses):
+def edit_courses_flask_form_builder(course_list):
     class ClassesFlaskForm(EditCoursesFlaskForm):
         pass
 
-    for i, course in enumerate(courses):
+    for i, course in enumerate(course_list):
         print(f'Creating course {i} forms')
         setattr(ClassesFlaskForm, f'is_monitored_course_{i}', BooleanField(label='Is monitored'))
         setattr(ClassesFlaskForm, f'auto_email_course_{i}', BooleanField(label='Auto email'))
         setattr(ClassesFlaskForm, f'short_name_course_{i}', StringField(label='Short name',
                                                                         default=course,
                                                                         render_kw={'readonly': True}))
-        courses.append({f'is_monitored_course_{i}': getattr(ClassesFlaskForm, f'is_monitored_course_{i}'),
-                        f'auto_email_course_{i}': getattr(ClassesFlaskForm, f'auto_email_course_{i}'),
-                        f'short_name_course_{i}': getattr(ClassesFlaskForm, f'short_name_course_{i}')})
+        ClassesFlaskForm.courses.append(
+            {f'is_monitored_course_{i}': getattr(ClassesFlaskForm, f'is_monitored_course_{i}'),
+             f'auto_email_course_{i}': getattr(ClassesFlaskForm, f'auto_email_course_{i}'),
+             f'short_name_course_{i}': getattr(ClassesFlaskForm, f'short_name_course_{i}')})
 
     print('Returning course form')
     return ClassesFlaskForm()
