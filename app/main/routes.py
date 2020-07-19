5aa90b147680e63de64ffc4b8ef0564635eca7b3
Paul Savala <paulsavala@gmail.com>
2020-07-19 13:23:28 -0500

    working on views

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask import render_template, current_app
+from flask import render_template, current_app, flash
 from flask_login import current_user, login_required
 
 from app import db
@@ -46,50 +46,10 @@ def index():
         # Get courses again (to reflect changes)
         courses = Courses.query.filter_by(instructor_id=current_user.id).all()
         form.num_courses = len(courses)
-        # # If the refresh button courses was clicked, talk to the API to get the courses from the LMs
-        # if form.refresh_courses.data:
-        #     print('Refresh courses')
-        #     # Get all courses for this instructor from the LMS
-        #     get_courses_url = resource_url(current_app.config['API_URL'], 'get_courses_by_instructor')
-        #     data = {'lms_token': current_user.lms_token,
-        #             'semester': current_app.config['SEMESTER'],
-        #             'instructor_lms_id': current_user.lms_id}
-        #     courses_resp = requests.post(get_courses_url, json=data).json()
-        #
-        #     # Check to see if they're already in the db
-        #     db_course_lms_ids = set([str(c.lms_id) for c in courses])
-        #     lms_course_lms_ids = set([str(c['lms_id']) for c in courses_resp])
-        #
-        #     new_course_ids = lms_course_lms_ids - db_course_lms_ids
-        #     new_courses = [c for c in courses_resp if c['lms_id'] in new_course_ids]
-        #     old_course_ids = db_course_lms_ids - lms_course_lms_ids
-        #     old_courses = [c for c in courses_resp if c['lms_id'] in old_course_ids]
-        #
-        #     # Add any that are not...
-        #     courses_to_add = ([Courses(lms_id=c['lms_id'],
-        #                                season=current_app.config['SEASON'],
-        #                                year=current_app.config['YEAR'],
-        #                                short_name=c['short_name'],
-        #                                long_name=c['long_name'],
-        #                                is_monitored=False,
-        #                                auto_email=False,
-        #                                instructor_id=current_user.id) for c in new_courses])
-        #     print(f'Adding to db: {[c.short_name for c in courses_to_add]}')
-        #     for c in courses_to_add:
-        #         db.session.add(c)
-        #
-        #     # ...and remove any that are in the db but not returned from the LMS
-        #     courses_to_remove = ([Courses.query.filter_by(id=c.id) for c in old_courses])
-        #     print(f'Deleting from db: {[c.short_name for c in courses_to_remove]}')
-        #     for c in courses_to_remove:
-        #         db.session.remove(c)
-        #
-        #     # Get courses again (to reflect changes)
-        #     courses = Courses.query.filter_by(instructor_id=current_user.id).all()
-        #     form.num_courses = len(courses)
 
     # Fill in the appropriate values for monitored and auto email
     for i, c in enumerate(courses):
+        # Submit values (totally separate from display values)
         getattr(form, f'is_monitored_{i}').default = c.is_monitored
         getattr(form, f'auto_email_{i}').default = c.auto_email
 
@@ -101,7 +61,7 @@ def about():
     return render_template('main/about.html')
 
 
-@bp.route('/settings')
+@bp.route('/settings', methods=['GET', 'POST'])
 def settings():
     refresh_courses_form = RefreshCoursesFlaskForm()
     if refresh_courses_form.validate_on_submit():
@@ -143,5 +103,6 @@ def settings():
         print(f'Deleting from db: {[c.short_name for c in courses_to_remove]}')
         for c in courses_to_remove:
             db.session.remove(c)
+        flash('Courses updated')
 
     return render_template('main/settings.html', refresh_courses_form=refresh_courses_form)
